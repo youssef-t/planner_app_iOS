be94fe6bc0e1d04dfb4f6e591db2eb66eec01220
Youssef Taleb <yt621671@gr10-07.esirem-ad.ad.u-bourgogne.fr>
2021-01-05 13:58:13 +0100

    Initialize project

---
+++ planner_app_iOS./planner_app_iOS./ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  planner_app_iOS.
+//
+//  Created by Youssef Taleb on 05/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
