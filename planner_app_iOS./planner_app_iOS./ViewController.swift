79da338e3a7a1b7162c394d60893791a4937f5dd
Youssef Taleb <yt621671@gr10-07.esirem-ad.ad.u-bourgogne.fr>
2021-01-05 17:24:59 +0100

    New StoryBoards and ViewControllers added - need to modify them to navigate between them

--- planner_app_iOS./planner_app_iOS./ViewController.swift
+++ planner_app_iOS./planner_app_iOS./ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var todayTasks: UIButton!
+    @IBOutlet weak var allPlanning: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func clickTodayTasks(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func clickAllPlanning(_ sender: UIButton) {
+        present(ViewControllerAllPlanning(), animated: true)
+        //let 
+    }
+}
 
 
+
+class Task {
+    var date = Date()
+    var hour: Int
+    var minute: Int
+    
+    var taskName: String
+    //small, medium, urgent
+    var taskImportance: String
+    var taskImportanceValueInt: Int
+    
+    init(_ date_l: Date, _ hour_l: Int = 12, _ minute_l: Int = 0, _ taskName_l: String, _ taskImportance_l: String = "urgent"){
+        date = date_l
+        hour = hour_l
+        minute = minute_l
+        taskName = taskName_l
+        taskImportance = taskImportance_l
+        if taskImportance == "small" {
+            taskImportanceValueInt = 0
+        } else if taskImportance == "medium" {
+            taskImportanceValueInt = 1
+        } else {
+            taskImportanceValueInt = 2 //urgent
+        }
+        //var datePicker = UIDatePicker()
+        
+    }
+    
 }
 
